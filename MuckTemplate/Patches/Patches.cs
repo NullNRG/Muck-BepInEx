c51dd5043bf11d1b4ce1105a758b095d716121c9
NullNRG <63321722+NullNRG@users.noreply.github.com>
2021-06-06 14:55:33 -0700

    Add files via upload

---
+++ MuckTemplate/Patches/Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace $safeprojectname$.Patches
+{
+    [HarmonyPatch]
+    public static class Patches
+    {
+		
+    }
+}
